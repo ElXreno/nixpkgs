0b046baaa829854e6cdfd98ddb307dcdd444dc2c
ElXreno <elxreno@gmail.com>
2021-11-18 16:22:36 +0300

    pgmodeler: Add desktop item

--- pkgs/applications/misc/pgmodeler/default.nix
+++ pkgs/applications/misc/pgmodeler/default.nix
@@ -5,6 +5,8 @@
 , pkg-config
 , qmake
 , qtwayland
+, makeDesktopItem
+, copyDesktopItems
 , qtsvg
 , postgresql
 }:
@@ -20,11 +22,27 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Km4PWvbIzgc1Kxsp26HYLCA4OkCfOsGWsdWYLmWf/NA=";
   };
 
-  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook ];
+  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook copyDesktopItems ];
+  desktopItems = [
+    (makeDesktopItem {
+      name = "pgModeler";
+      exec = "${pname}";
+      icon = "${pname}";
+      desktopName = "pgModeler";
+      genericName = "PostgreSQL Database Modeler";
+      comment = "Create and deploy PostgreSQL database models";
+      categories = [ "Development" ];
+    })
+  ];
+
   qmakeFlags = [ "pgmodeler.pro" "CONFIG+=release" ];
 
   # todo: libpq would suffice here. Unfortunately this won't work, if one uses only postgresql.lib here.
   buildInputs = [ postgresql qtsvg qtwayland ];
+
+  postInstall = ''
+    install -Dm644 $out/share/pgmodeler/conf/pgmodeler_logo.png $out/share/pixmaps/${pname}.png
+  '';
 
   meta = with lib; {
     description = "A database modeling tool for PostgreSQL";
