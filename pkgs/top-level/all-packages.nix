881f349a06925a7c37c4677a0c9f049eeb1b7203
ElXreno <elxreno@gmail.com>
2023-01-01 15:39:35 +0300

    upscaler: Init at 1.1.2

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -15986,6 +15986,8 @@ with pkgs;
 
   unigine-superposition = libsForQt5.callPackage ../applications/graphics/unigine-superposition { };
 
+  upscaler = callPackage ../applications/graphics/upscaler { };
+
   uuagc = haskell.lib.compose.justStaticExecutables haskellPackages.uuagc;
 
   valentina = libsForQt5.callPackage ../applications/misc/valentina { };
