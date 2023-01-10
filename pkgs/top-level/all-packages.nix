a49b8f55d87820c0ee2026c614a8600289d06933
ElXreno <elxreno@gmail.com>
2023-01-10 05:09:23 +0300

    xclicker: Init at 1.4.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36328,6 +36328,8 @@ with pkgs;
 
   xchm = callPackage ../applications/misc/xchm { };
 
+  xclicker = callPackage ../tools/X11/xclicker { };
+
   inherit (xorg) xcompmgr;
 
   x-create-mouse-void = callPackage ../applications/window-managers/x-create-mouse-void { };
