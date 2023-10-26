15465681a11655d9e39b1bddefd5eeee4b02e791
ElXreno <elxreno@gmail.com>
2023-10-26 04:54:02 +0300

    Revert "pgmodeler: Add desktop item"
    This reverts commit e787c4772b55a0ec1d5c5ad791fed7d23897d9b1.

--- pkgs/applications/misc/pgmodeler/default.nix
+++ pkgs/applications/misc/pgmodeler/default.nix
@@ -5,8 +5,6 @@
 , pkg-config
 , qmake
 , qtwayland
-, makeDesktopItem
-, copyDesktopItems
 , qtsvg
 , postgresql
 }:
@@ -22,27 +20,11 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-Km4PWvbIzgc1Kxsp26HYLCA4OkCfOsGWsdWYLmWf/NA=";
   };
 
-  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook copyDesktopItems ];
-  desktopItems = [
-    (makeDesktopItem {
-      name = "pgModeler";
-      exec = "${pname}";
-      icon = "${pname}";
-      desktopName = "pgModeler";
-      genericName = "PostgreSQL Database Modeler";
-      comment = "Create and deploy PostgreSQL database models";
-      categories = [ "Development" ];
-    })
-  ];
-
+  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook ];
   qmakeFlags = [ "pgmodeler.pro" "CONFIG+=release" ];
 
   # todo: libpq would suffice here. Unfortunately this won't work, if one uses only postgresql.lib here.
   buildInputs = [ postgresql qtsvg qtwayland ];
-
-  postInstall = ''
-    install -Dm644 $out/share/pgmodeler/conf/pgmodeler_logo.png $out/share/pixmaps/${pname}.png
-  '';
 
   meta = with lib; {
     description = "A database modeling tool for PostgreSQL";
